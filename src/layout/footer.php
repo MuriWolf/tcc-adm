ea868adda63d56888642c6185ebd725e0677b738
MuriWolf <murillop.o06@gmail.com>
2023-05-15 22:22:02 -0300

    changes

---
+++ src/layout/footer.php
@@ -0,0 +1,15 @@
+<footer class="bg-dark-blue--footer w-full h-24">
+    <div class="container flex flex-col gap-2 md:gap-0 p-6 md:flex-row justify-around mx-auto items-center h-full">
+        <h4 class="text-white text-lg">© 2023 South Tech</h4>
+        <nav class="flex items-center">
+            <ul class="flex gap-4">
+                <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400" href="#"><i
+                            class="fa-brands fa-youtube fa-2xl"></i></a></li>
+                <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400" href="#"><i
+                            class="fa-brands fa-facebook fa-2xl"></i></a></li>
+                <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400" href="#"><i
+                            class="fa-brands fa-instagram fa-2xl"></i></a></li>
+            </ul>
+        </nav>
+    </div>
+</footer>
---
+++ src/layout/header.php
@@ -0,0 +1,27 @@
+    <header class="hero h-screen max-h-[46rem]">
+        <div class="container flex justify-between items-center bg-black bg-opacity-25 py-6 px-6 min-w-full">
+
+            <a href="#">
+                <div style="height: 60px; width: 60px; border-radius: 100px; background-color: white;"></div>
+            </a> <!-- LOGO IMAGE -->
+
+            <nav class="flex items-center">
+                <ul class="hidden sm:flex gap-4">
+                    <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400"
+                            href="#"><i class="fa-brands fa-youtube fa-2xl"></i></a></li>
+                    <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400"
+                            href="#"><i class="fa-brands fa-facebook fa-2xl"></i></a></li>
+                    <li class="transition-all"><a class="text-white hover:text-yellow-400 focus:text-yellow-400"
+                            href="#"><i class="fa-brands fa-instagram fa-2xl"></i></a></li>
+                </ul>
+                <ul class="flex gap-4 ml-8 items-center">
+                    <li><a href="pages/sobre-nos.html"
+                            class="text-white text-lg hover:text-yellow-400 focus:text-yellow-400 hover:font-bold focus:font-bold">Sobre
+                            nós</a></li>
+                    <li><a href="./pages/pagina-cadastro.html"
+                            class="bg-white inline-block py-2 px-3 rounded hover:shadow hover:bg-yellow-400 focus:bg-yellow-400 hover:font-bold focus:font-bold transition-all focus:scale-105">Cadastro</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        "
